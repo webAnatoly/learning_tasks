cd552b10cff01b8fc6f5bb8a829f1a4400df720b
Tolik Dem <tolik.demianov@gmail.com>
2018-03-11 16:35:52 +0300

    Добавил решение Задачи 6

--- src/practise.js
+++ src/practise.js
@@ -125,6 +125,32 @@ const fizzBuzz = (begin, end) => {
 
 // Задача 6 Счастливые числа
 // https://ru.hexlet.io/challenges/programming_basics_happy_numbers
+const isHappyNumber = (number) => {
+
+  const sumOfSquareDigits = (n) => {
+    // функция принимает на вход число и возвращает "сумму квадратов цифр" этого числа
+    return String(n).split('').map(n => Number(n)**2).reduce((a,b) => a+b);
+  }
+
+  let counter = 0;
+
+  while (counter < 10) {
+    if (number == 1) {
+      return true;
+    } else if (number == 0) {
+      return false;
+    } else if (number <= 9) {
+      number = number**2;
+    } else {
+      number = sumOfSquareDigits(number);
+    }
+    counter++;
+  }
+  return ('A lot of iteration');
+}
+
+assert.equal(isHappyNumber(7), true); // должно вернуть true
+assert.equal(isHappyNumber(798765432), 'A lot of iteration'); // должно вернуть 'A lot of iteration'
 
 // Задача 7 Переворот числа
 
